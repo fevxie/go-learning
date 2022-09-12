8fa0420211a846688d7d502f304a306710fab55b
Qing Wang <wangqing985@gmail.com>
2022-09-12 19:46:01 +0800

    add k8s learning codes

---
+++ JSON.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+)
+
+type Person struct {
+    ID        int
+    FirstName string `json: "name"`
+    LastName  string
+    Address   string `json: "address,omitempty"`
+}
+
+type Employee struct {
+    Person
+    Manager int
+}
+
+type Contractor struct {
+    Person
+    CompanyID int
+}
+
+func main() {
+    employees := []Employee{
+        Employee{
+            Person: Person{
+                LastName:  "Doe",
+                FirstName: "John",
+            },
+        },
+        Employee{
+            Person: Person{
+                LastName:  "Cambell",
+                FirstName: "David",
+            },
+        },
+    }
+    data, _ := json.Marshal(employees)
+    fmt.Printf("%s\n", data)
+
+    var decoded []Employee
+    json.Unmarshal(data, &decoded)
+    fmt.Printf("%v", decoded)
+}
